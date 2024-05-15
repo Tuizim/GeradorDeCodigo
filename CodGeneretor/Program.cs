963ad8b268cf5baa89aeee6200c1ae668c4b9069
artur.trombim <artur.trombim@itravel.com.br>
2024-05-15 17:47:47 -0300

    First Commit: [ADD] Arquivo Base

---
+++ CodGeneretor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Xml.Linq;
+
+namespace CodGeneretor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string xmlFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Main.xml");
+
+            XmlFinder.FindAtributoHttpXml(xmlFilePath, "add", "value");
+            XmlFinder.FindAtributoXml(xmlFilePath, "add", "connectionString");
+            XmlFinder.FindElementoHttpXml(xmlFilePath, "setting", "value");
+            XmlFinder.FindAtributoHttpXml(xmlFilePath, "endpoint", "address");
+
+        }
+    }
+}
---
+++ CodGeneretor/XmlFinder.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+using System.Xml.Linq;
+
+namespace CodGeneretor
+{
+    class XmlFinder
+    {
+        #region METODOS DE BUSCA
+
+        /// <summary>
+        /// Metodo para pegar ATRIBUTO HTTP especifico no XML
+        /// ( FilePath , Decendente , Atributo  )
+        /// </summary>
+        public static void FindAtributoHttpXml(string filePath, string decendete, string atributo)
+        {
+
+            if (File.Exists(filePath))
+            {
+                XElement xmlDoc = XElement.Load(filePath);
+                foreach (XElement setting in xmlDoc.Descendants(decendete))
+                {
+                    XAttribute nome = setting.Attribute(atributo);
+                    if (nome != null)
+                    {
+                        if (nome.Value.StartsWith("http"))
+                        {
+                            Console.WriteLine(nome.Value);
+                        }
+
+                    }
+                }
+            }
+            else { Console.WriteLine($@"PATH incorreto: {filePath}"); }
+        }
+
+        /// <summary>
+        /// Metodo para pegar ELEMENTO HTTP especifico no XML
+        /// ( FilePath , Decendente , Atributo  )
+        /// </summary>
+        public static void FindElementoHttpXml(string filePath, string decendete, string elemento)
+        {
+
+            if (File.Exists(filePath))
+            {
+                XElement xmlDoc = XElement.Load(filePath);
+                foreach (XElement setting in xmlDoc.Descendants(decendete))
+                {
+                    XElement nome = setting.Element(elemento);
+                    if (nome != null)
+                    {
+                        if (nome.Value.StartsWith("http"))
+                        {
+                            Console.WriteLine(nome.Value);
+                        }
+
+                    }
+                }
+            }
+            else { Console.WriteLine($@"PATH incorreto: {filePath}"); }
+        }
+
+        /// <summary>
+        /// Metodo para pegar ELEMENTO especifico no XML
+        /// ( FilePath , Decendente , Atributo  )
+        /// </summary>
+        public static void FindElementoXml(string filePath, string decendete, string elemento)
+        {
+
+            if (File.Exists(filePath))
+            {
+                XElement xmlDoc = XElement.Load(filePath);
+                foreach (XElement setting in xmlDoc.Descendants(decendete))
+                {
+                    XElement nome = setting.Element(elemento);
+                    if (nome != null)
+                    {
+                        Console.WriteLine(nome.Value);
+                    }
+                }
+            }
+            else { Console.WriteLine($@"PATH incorreto: {filePath}"); }
+        }
+
+        /// <summary>
+        /// Metodo para pegar ATRIBUTO HTTP especifico no XML
+        /// ( FilePath , Decendente , Atributo  )
+        /// </summary>
+        public static void FindAtributoXml(string filePath, string decendete, string atributo)
+        {
+
+            if (File.Exists(filePath))
+            {
+                XElement xmlDoc = XElement.Load(filePath);
+                foreach (XElement setting in xmlDoc.Descendants(decendete))
+                {
+                    XAttribute nome = setting.Attribute(atributo);
+                    if (nome != null)
+                    {
+                        Console.WriteLine(nome.Value);
+                    }
+                }
+            }
+            else { Console.WriteLine($@"PATH incorreto: {filePath}"); }
+        }
+        #endregion
+    }
+}
