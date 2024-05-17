5ae3844b4f59966fc9b4196d596f880016c37cba
Gabriel Bonil <gabriel.bonil@itravel.com.br>
2024-05-17 15:47:15 -0300

    [ADD/UPD] Teste de atualização e refatoração do código

--- CodGeneretor/Program.cs
+++ CodGeneretor/Program.cs
@@ -8,13 +8,32 @@ namespace CodGeneretor
     {
         static void Main(string[] args)
         {
-            string xmlFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Main.xml");
+            //string xmlFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Main.xml");
+            string xmlFilePath = @"C:\Users\gabriel.bonil\Documents\WTX WebConfig\FilipeWebConfig - Copy.xml";
+            Console.WriteLine("Path: ", xmlFilePath);
 
-            XmlFinder.FindAtributoHttpXml(xmlFilePath, "add", "value");
-            XmlFinder.FindAtributoXml(xmlFilePath, "add", "connectionString");
-            XmlFinder.FindElementoHttpXml(xmlFilePath, "setting", "value");
-            XmlFinder.FindAtributoHttpXml(xmlFilePath, "endpoint", "address");
+            if (!File.Exists(xmlFilePath))
+            {
+                Console.WriteLine($@"PATH incorreto: {xmlFilePath}");
+            }
+            else
+            {
+                XElement xmlDoc = XElement.Load(xmlFilePath);
 
+                Console.WriteLine("\n\nFind Element HTTP Settings/value \n");
+                XmlFinder.FindElementoHttpXml(xmlDoc, "setting", "value");
+
+                Console.WriteLine("\n\nFind Attribute add/value \n");
+                XmlFinder.FindAtributoHttpXml(xmlDoc, "add", "value");
+
+                Console.WriteLine("\n\nFind Attribute add/connectionString \n");
+                XmlFinder.FindAtributoXml(xmlDoc, "add", "connectionString");
+
+                Console.WriteLine("\n\nFind Attribute endpoint/address \n");
+                XmlFinder.FindAtributoHttpXml(xmlDoc, "endpoint", "address");
+
+                xmlDoc.Save(xmlFilePath);
+            }
         }
     }
 }
--- CodGeneretor/XmlFinder.cs
+++ CodGeneretor/XmlFinder.cs
@@ -12,96 +12,79 @@ namespace CodGeneretor
         /// Metodo para pegar ATRIBUTO HTTP especifico no XML
         /// ( FilePath , Decendente , Atributo  )
         /// </summary>
-        public static void FindAtributoHttpXml(string filePath, string decendete, string atributo)
+        public static void FindAtributoHttpXml(XElement xmlDoc, string decendete, string atributo)
         {
-
-            if (File.Exists(filePath))
+            foreach (XElement setting in xmlDoc.Descendants(decendete))
             {
-                XElement xmlDoc = XElement.Load(filePath);
-                foreach (XElement setting in xmlDoc.Descendants(decendete))
+                XAttribute nome = setting.Attribute(atributo);
+                if (nome != null)
                 {
-                    XAttribute nome = setting.Attribute(atributo);
-                    if (nome != null)
+                    if (nome.Value.StartsWith("http"))
                     {
-                        if (nome.Value.StartsWith("http"))
-                        {
-                            Console.WriteLine(nome.Value);
-                        }
-
+                        Console.WriteLine(nome.Value); // Era assim
+                        nome.Value = "Mudei";
+                        Console.WriteLine(nome.Value);
                     }
+
                 }
             }
-            else { Console.WriteLine($@"PATH incorreto: {filePath}"); }
         }
 
         /// <summary>
         /// Metodo para pegar ELEMENTO HTTP especifico no XML
         /// ( FilePath , Decendente , Atributo  )
         /// </summary>
-        public static void FindElementoHttpXml(string filePath, string decendete, string elemento)
+        public static void FindElementoHttpXml(XElement xmlDoc, string decendete, string elemento)
         {
-
-            if (File.Exists(filePath))
+            foreach (XElement setting in xmlDoc.Descendants(decendete))
             {
-                XElement xmlDoc = XElement.Load(filePath);
-                foreach (XElement setting in xmlDoc.Descendants(decendete))
+                XElement nome = setting.Element(elemento);
+                if (nome != null)
                 {
-                    XElement nome = setting.Element(elemento);
-                    if (nome != null)
+                    if (nome.Value.StartsWith("http"))
                     {
-                        if (nome.Value.StartsWith("http"))
-                        {
-                            Console.WriteLine(nome.Value);
-                        }
-
+                        Console.WriteLine(nome.Value); // Era assim
+                        nome.Value = "Mudei";
+                        Console.WriteLine(nome.Value);
                     }
+
                 }
             }
-            else { Console.WriteLine($@"PATH incorreto: {filePath}"); }
         }
 
         /// <summary>
         /// Metodo para pegar ELEMENTO especifico no XML
         /// ( FilePath , Decendente , Atributo  )
         /// </summary>
-        public static void FindElementoXml(string filePath, string decendete, string elemento)
+        public static void FindElementoXml(XElement xmlDoc, string decendete, string elemento)
         {
-
-            if (File.Exists(filePath))
+            foreach (XElement setting in xmlDoc.Descendants(decendete))
             {
-                XElement xmlDoc = XElement.Load(filePath);
-                foreach (XElement setting in xmlDoc.Descendants(decendete))
+                XElement nome = setting.Element(elemento);
+                if (nome != null)
                 {
-                    XElement nome = setting.Element(elemento);
-                    if (nome != null)
-                    {
-                        Console.WriteLine(nome.Value);
-                    }
+                    Console.WriteLine(nome.Value);
                 }
             }
-            else { Console.WriteLine($@"PATH incorreto: {filePath}"); }
         }
 
         /// <summary>
         /// Metodo para pegar ATRIBUTO HTTP especifico no XML
         /// ( FilePath , Decendente , Atributo  )
         /// </summary>
-        public static void FindAtributoXml(string filePath, string decendete, string atributo)
+        public static void FindAtributoXml(XElement xmlDoc, string decendete, string atributo)
         {
-
-            if (File.Exists(filePath))
+            foreach (XElement setting in xmlDoc.Descendants(decendete))
             {
-                XElement xmlDoc = XElement.Load(filePath);
-                foreach (XElement setting in xmlDoc.Descendants(decendete))
+                XAttribute nome = setting.Attribute(atributo); //Não tem como fazer alteração direta, mensagem diz ser por causa da falta de comparação de ver se é Vazio
+                if (nome != null)
                 {
-                    XAttribute nome = setting.Attribute(atributo);
-                    if (nome != null)
-                    {
-                        Console.WriteLine(nome.Value);
-                    }
+                    Console.WriteLine(nome.Value + "\n\n"); //Era só isso
+                    nome.Value = "Mudei"; //Caso der errado temos o replace: https://learn.microsoft.com/en-us/dotnet/api/system.xml.linq.xelement.replaceattributes?view=net-8.0
+                    Console.WriteLine(nome.Value + "\n\n");
+                    Console.WriteLine(nome + "\n\n");
                 }
             }
-            else { Console.WriteLine($@"PATH incorreto: {filePath}"); }
         }
         #endregion
     }
